8161a7a4cbaeb66ac8371769a494e2ea3a817d76
Betu55 <se.bemenet.semegnew@gmail.com>
2020-05-27 15:41:21 +0200

    The about page is complete with some js added to make the images more eye catching.

--- Assets/js/myJS.js
+++ Assets/js/myJS.js
@@ -5,3 +5,35 @@ function setNewImage(){
 function setOldImage(){
 	document.getElementById("brand").src = "Assets/Images/Brand5.png";
 }
+
+function newImage1(){
+    document.getElementById("fbook").src = "Assets/Images/Brand9.png";
+}
+
+function oldImage1(){
+    document.getElementById("fbook").src = "Assets/Images/Brand9-gray.png";
+}
+
+function newImage2(){
+    document.getElementById("wapp").src = "Assets/Images/Brand10.png";
+}
+
+function oldImage2(){
+    document.getElementById("wapp").src = "Assets/Images/Brand10-gray.png";
+}
+
+function newImage3(){
+    document.getElementById("ytube").src = "Assets/Images/Brand11.png";
+}
+
+function oldImage3(){
+    document.getElementById("ytube").src = "Assets/Images/Brand11-gray.png";
+}
+
+function newImage4(){
+    document.getElementById("tweet").src = "Assets/Images/Brand12.png";
+}
+
+function oldImage4(){
+    document.getElementById("tweet").src = "Assets/Images/Brand12-gray.png";
+}
